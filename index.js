d11e2612c3d6ed2a37d18c6032a3628d1336779a
VadimKurachynski <kurachynski@gmail.com>
2023-02-09 21:05:55 +0300

    2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const http = require('http')
-http
-    .createServer(function (request, response) {
-        response.end('Hello NodeJS!')
-    })
-    .listen(5002, '127.0.0.1', function () {
-        console.log(
-            'Сервер начал прослушивание запросов на порту 5002'
-        )
-    })
+
+require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN) //сюда помещается токен, который дал botFather
+bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
+bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
+bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+bot.launch() // запуск бота
