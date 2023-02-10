3af99b679082cf1608406e777865faaf1c227c1e
VadimKurachynski <kurachynski@gmail.com>
2023-02-10 21:12:43 +0300

    1

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
-const {env} = require("telegraf/typings/util");
-require('dotenv').config()
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
 PORT=process.env.PORT;
+const app = express();
+const axios = require('axios');
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const text='Привет!'
+bot.start(ctx => {
+    ctx.reply('Welcome, bro')
+})
+bot.on('text', ctx => {
+    ctx.reply('just text')
+})
+bot.launch()
 
-const app = express()
 
-app.get('/', (req, res) => {
+app.get('/api/text', (req, res) => {
     res.send('Hello. I changed this text')
+    PostText();
 })
 
+async function PostText() {
+    try {
+        const response = await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${text}`);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-app.get('/api/theme', isAuth, dbPg.getQuestionAll);//access to questions
 
 
 
