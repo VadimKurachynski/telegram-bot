a918bad1972a17b191b615c7d94607edd9218365
VadimKurachynski <kurachynski@gmail.com>
2023-02-10 21:37:50 +0300

    45

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 PORT=process.env.PORT;
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 const axios = require('axios');
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const text='Привет!'
+
 bot.start(ctx => {
     ctx.reply('Welcome, bro')
 })
@@ -14,22 +19,38 @@ bot.on('text', ctx => {
 })
 bot.launch()
 
+let textSend='Привет!';
+let token=process.env.BOT_TOKEN;
+let chatId=process.env.CHAT_ID;
 
 app.get('/api/text', (req, res) => {
     res.send('Hello. I changed this text')
-    PostText();
+    PostText(token,chatId,"hfjfjhfjh");
 })
 
-async function PostText() {
+
+
+async function PostText(token,chatId,textSend ) {
     try {
-        const response = await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${text}`);
-        console.log(response);
+        await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+            chat_id: chatId,
+            text: textSend
+        });
     } catch (error) {
         console.error(error);
     }
 }
 
 
+
+
+app.get('/send', (req, res) => {
+    axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+        chat_id: process.env.CHAT_ID,
+       text: 'Telegram bot'
+    });
+    res.send('send message');
+});
 
 
 
