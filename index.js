b8d49b18b25465e7578ff59e0500e9ec0aea0229
VadimKurachynski <kurachynski@gmail.com>
2023-02-10 15:49:00 +0300

    ser

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-
+const express = require('express');
+const {env} = require("telegraf/typings/util");
 require('dotenv').config()
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.BOT_TOKEN) //сюда помещается токен, который дал botFather
+PORT=process.env.PORT;
 
-bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
-bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
-bot.launch() // запуск бота
+const app = express()
 
-bot.on("message", (ctx) => {
-    console.log(ctx.message.from.first_name)// имя пользователя, который написал
-    console.log(ctx.message.text) // текст который пользователь отправил
-    console.log(ctx.message.chat.id)//
+app.get('/', (req, res) => {
+    res.send('Hello. I changed this text')
+})
 
-    bot.telegram.sendMessage(ctx.message.chat.id,
-        `Привет, ${ctx.message.from.first_name}\n` +
-        `Твой id: ${ctx.message.from.id}`)
 
-});
+app.get('/api/theme', isAuth, dbPg.getQuestionAll);//access to questions
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
