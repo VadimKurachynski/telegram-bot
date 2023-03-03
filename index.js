04b511cf41235373f9f8844159b7201ae479fe24
VadimKurachynski <kurachynski@gmail.com>
2023-03-03 22:36:51 +0300

    45

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const fs = require('fs');
 const path = require("path");
 const FileSizeNorm = 15000000;
+app.use( '/memo',express.static("./static/files"));
 const pathJsonInfo ="./static/json/info.json";
 let token = process.env.BOT_TOKEN;
 bot.launch();
-app.use(express.static("./static/files"));
+
+
 //-----------------------
 app.get("/api2/files", appController.ApiFiles_get);
 app.get("/api2/allfilesdelete", appController.ApiFilesDelete_get);
@@ -108,7 +110,6 @@ function downloadFile(url, filename, callback) {
     })
 
 }
-
 
 
 app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
